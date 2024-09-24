c1a7d52c63e2949310cc03ebaa2a7040c272fe37
theabhipatel <theabhipatel.co@gmail.com>
2024-09-24 12:05:52 +0530

    test eslint rules and prettier tested successfully

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="container bg-blue-400 max-w-screen-">
-      <h1>Developers Blog</h1>
-      <p>For </p>
+    <div className="container w-full bg-black">
+      <div className="flex h-dvh w-full flex-col items-center justify-center gap-2">
+        <h1 className="text-3xl font-semibold">Developers Blog</h1>
+        <p className="text-sm tracking-widest">For Developer by Developer</p>
+
+        <Button>Get Started</Button>
+      </div>
     </div>
   );
 };
