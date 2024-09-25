d0aa56e3abadf7a1cc2d7f25a747f4a08a93ed76
theabhipatel <theabhipatel.co@gmail.com>
2024-09-25 11:28:39 +0530

    create an empty new blog page has been created

---
+++ src/app/(user)/new-blog/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewBlog = () => {
+  return (
+    <div className="flex h-[70vh] w-full items-center justify-center">
+      <h1 className="text-4xl font-semibold tracking-widest">New Blog</h1>
+    </div>
+  );
+};
+
+export default NewBlog;
