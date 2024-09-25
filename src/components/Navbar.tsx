097cb37b0b0c9457672345f4caaa28b027ac5e29
theabhipatel <theabhipatel.co@gmail.com>
2024-09-25 10:53:23 +0530

    feat navbar component made  fixed on top and gave backdorp blur

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,18 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header className="container flex justify-between bg-slate-800">
-      <div className="text-2xl font-semibold">Developers Blog</div>
-      <nav className="flex items-end gap-5 text-sm text-primary-foreground">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/blogs"}>Blogs</Link>
-        <Link href={"/category"}>Category</Link>
-        <Link href={"/about"}>About</Link>
-      </nav>
+    <header className="fixed top-0 mx-auto w-full border-b border-gray-200/10 bg-slate-800/50 backdrop-blur-md">
+      <div className="container flex h-16 w-full justify-between">
+        <div className="flex items-center text-3xl font-semibold">
+          <Link href={"/"}>Developers Blog</Link>
+        </div>
+        <nav className="flex items-end gap-5 pb-2 text-sm text-primary-foreground">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/blogs"}>Blogs</Link>
+          <Link href={"/category"}>Category</Link>
+          <Link href={"/about"}>About</Link>
+        </nav>
+      </div>
     </header>
   );
 };
