0c964460d475716e1ab6895484d59fd26fdfedb9
theabhipatel <theabhipatel.co@gmail.com>
2024-09-25 11:48:41 +0530

    feat added search box in navbar and updated navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,23 @@ const Navbar = () => {
         <div className="flex items-center text-3xl font-semibold">
           <Link href={"/"}>Developers Blog</Link>
         </div>
-        <nav className="flex items-end gap-5 pb-2 text-sm text-primary-foreground">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/blogs"}>Blogs</Link>
-          <Link href={"/category"}>Category</Link>
-          <Link href={"/about"}>About</Link>
-        </nav>
+
+        <div className="flex items-center">
+          <div className="flex h-9 w-52 items-center rounded-full border border-gray-200/20 px-5">
+            <p className="text-sm text-gray-300/40">Search for anything..</p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <nav className="flex items-end gap-5 text-sm text-primary-foreground">
+            <Link href={"/blogs"}>Blogs</Link>
+            <Link href={"/category"}>Category</Link>
+            <Link href={"/new-blog"}>Write</Link>
+          </nav>
+          <div>
+            <div className="h-9 w-9 rounded-full border border-gray-200/50"></div>
+          </div>
+        </div>
       </div>
     </header>
   );
