23fb4c6f0f56457dd122444a1b8925ac3bbe411c
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-09-29 14:28:47 +0530

    update add new blog page and added slug input as well

--- src/app/(user)/new-blog/page.tsx
+++ src/app/(user)/new-blog/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -8,12 +8,13 @@ import { useToast } from "@/hooks/use-toast";
 const NewBlog = () => {
   const { toast } = useToast();
   const [title, setTitle] = useState("");
+  const [slug, setSlug] = useState("");
   const [thumbnail, setThumbnail] = useState("");
   const [mdContent, setMdContent] = useState("**Namaskar Developers!!**");
   const [isEditorFullScreen, setIsEditorFullScreen] = useState(false);
 
   const handleSave = () => {
-    if (!title || !thumbnail || !mdContent) {
+    if (!title || !slug || !thumbnail || !mdContent) {
       toast({
         title: "All field Required!!",
         description: "Title, Thumbnail and Content is Required to save.",
@@ -28,6 +29,7 @@ const NewBlog = () => {
       {
         id: `bid${Date.now()}`,
         title,
+        slug,
         thumbnail,
         content: mdContent,
         createdAt: Date.now(),
@@ -40,6 +42,17 @@ const NewBlog = () => {
     setTitle("");
     setThumbnail("");
     setMdContent("**Namaskar Developers!!**");
+  };
+
+  /** Handling Title and Slug */
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const slug = e.target.value.split(" ").join("-");
+    setTitle(e.target.value);
+    setSlug(slug);
+  };
+  const handleSlugChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const slug = e.target.value.split(" ").join("-");
+    setSlug(slug);
   };
 
   return (
@@ -64,7 +77,17 @@ const NewBlog = () => {
             id="title"
             name="title"
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={handleTitleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="slug">Slug</label>
+          <Input
+            className=""
+            id="slug"
+            name="slug"
+            value={slug}
+            onChange={handleSlugChange}
           />
         </div>
         <div>
