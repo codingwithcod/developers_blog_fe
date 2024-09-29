b64745ebb8df13f7836172a143fc56b8dfb0ee04
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-09-29 14:27:22 +0530

    update BlogCard and used slug in navigation link instead of title

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -14,7 +14,7 @@ interface IProps {
 }
 
 const BlogCard: FC<IProps> = ({ blog }) => {
-  const { title, thumbnail, userName, reads, createdAt } = blog;
+  const { title, slug, thumbnail, userName, reads, createdAt } = blog;
 
   const handleOptionClick = (e: MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
@@ -22,7 +22,7 @@ const BlogCard: FC<IProps> = ({ blog }) => {
   };
 
   return (
-    <Link href={`/blogs/${title}`}>
+    <Link href={`/blogs/${slug}`}>
       <Card className="w-full border-none bg-background text-foreground">
         {/* ---> Thumbnail  */}
         <div className="overflow-hidden rounded-lg">
