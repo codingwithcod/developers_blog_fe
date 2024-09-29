88f32279b3811c6cd23cebf07713e4749f0aaf74
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-09-29 14:29:55 +0530

    update blogs page and added message and button if there is no blogs available

--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -2,15 +2,32 @@
 import React from "react";
 import { IBlog } from "@/interfaces/blog";
 import BlogCard from "@/components/BlogCard";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { HiPencilSquare } from "react-icons/hi2";
 
 const Blogs = () => {
   const localBlogs = JSON.parse(localStorage.getItem("blogs") ?? "[]") as IBlog[];
 
   return (
     <div className="container flex h-[60vh] w-full flex-col items-center">
-      <h1 className="my-20 text-5xl font-semibold tracking-widest">Blogs</h1>
-
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      {localBlogs.length === 0 && (
+        <div className="flex h-full w-full flex-col items-center justify-end">
+          <p className="text-2xl">There is no blogs available</p>
+          <p className="text-muted-foreground">You can also Write your own blogs</p>
+          <Link href={`/new-blog`}>
+            <Button
+              variant={"outline"}
+              size={"sm"}
+              className="mt-4 w-20 gap-1 rounded-full"
+            >
+              <HiPencilSquare className="text-sm" />
+              Write
+            </Button>
+          </Link>
+        </div>
+      )}
+      <div className="mt-24 grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
         {localBlogs.map((blog) => {
           return (
             <BlogCard
@@ -27,7 +44,7 @@ const Blogs = () => {
 export default Blogs;
 
 // eslint-disable-next-line
-const obj: IBlog[] = [
+const obj = [
   {
     id: `1727509555419`,
     title: "How to install and setup NGINX in Windows 11 [2024]",
