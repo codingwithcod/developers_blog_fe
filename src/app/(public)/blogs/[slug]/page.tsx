17029c00e75df449786a0c2741c7c04380b53087
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-09-29 20:32:24 +0530

    feat render markdown content dynamically using @uiw/react-md-editor library's preview component

--- src/app/(public)/blogs/[slug]/page.tsx
+++ src/app/(public)/blogs/[slug]/page.tsx
@@ -1,8 +1,12 @@
 "use client";
-import React, { FC, useState } from "react";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import rehypeHighlight from "rehype-highlight";
+import React, { FC, useEffect, useState } from "react";
+// import Markdown from "react-markdown";
+// import remarkGfm from "remark-gfm";
+// import rehypeHighlight from "rehype-highlight";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { IBlog } from "@/interfaces/blog";
+import MDEditor from "@uiw/react-md-editor";
 
 interface IProps {
   params: {
@@ -11,18 +15,34 @@ interface IProps {
 }
 
 const Blog: FC<IProps> = ({ params: { slug } }) => {
+  const query = useSearchParams();
   const decodedSlug = decodeURIComponent(slug);
+  const [mdContent, setMdContent] = useState("");
 
-  const [markdown, setMarkDown] = useState(mdContent2); //eslint-disable-line
+  const localBlogs = JSON.parse(localStorage.getItem("blogs") ?? "[]") as IBlog[];
+
+  useEffect(() => {
+    if (localBlogs.length > 0) {
+      const blog = localBlogs.find((blog) => blog.id === query.get("id"));
+      setMdContent(blog?.content ?? "");
+    }
+  }, []);
 
   return (
     <div className="container flex min-h-[70vh] w-full flex-col px-32 pt-20">
       <div>
         <p className="text-2xl font-semibold tracking-widest">
-          Blog / <span className="font-bold">{decodedSlug}</span>
+          <Link
+            href={`/blogs`}
+            className="duration-300 hover:text-blue-400 hover:underline"
+          >
+            Blogs
+          </Link>
+          / <span className="font-bold">{decodedSlug}</span>
         </p>
       </div>
-      <div className="prose mt-10 min-h-96 w-full max-w-none rounded-lg p-5 prose-headings:text-primary-foreground prose-p:text-primary-foreground prose-a:text-blue-300 prose-blockquote:bg-slate-100/10 prose-blockquote:px-5 prose-blockquote:py-[1px] prose-blockquote:text-orange-600 prose-strong:text-primary-foreground prose-ul:text-primary-foreground prose-table:text-primary-foreground prose-hr:text-red-500 dark:prose-h1:text-red-500">
+      {/* /** This is my research on how we can render makrdown with style using tailwind css. */}
+      {/* <div className="prose mt-10 min-h-96 w-full max-w-none rounded-lg p-5 prose-headings:text-primary-foreground prose-p:text-primary-foreground prose-a:text-blue-300 prose-blockquote:bg-slate-100/10 prose-blockquote:px-5 prose-blockquote:py-[1px] prose-blockquote:text-orange-600 prose-strong:text-primary-foreground prose-ul:text-primary-foreground prose-table:text-primary-foreground prose-hr:text-red-500 dark:prose-h1:text-red-500">
         <Markdown
           className={"w-full"}
           remarkPlugins={[remarkGfm]}
@@ -30,6 +50,12 @@ const Blog: FC<IProps> = ({ params: { slug } }) => {
         >
           {markdown}
         </Markdown>
+      </div> */}
+      <div className="mt-10">
+        <MDEditor.Markdown
+          source={mdContent}
+          style={{ whiteSpace: "pre-wrap", padding: 10 }}
+        />
       </div>
     </div>
   );
@@ -95,6 +121,7 @@ this is paragraph which is very long to read and write but i am testing it to do
 \`\`\`
 `;
 
+// eslint-disable-next-line
 const mdContent2 = `<!--rehype:ignore:start-->
 React Markdown Preview
 ===
