cd97b99f95115338616df9ebe18cfbc9f11b4f03
theabhipatel <theabhipatel.co@gmail.com>
2024-09-30 14:45:34 +0530

    feat created a basic login page

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      //   console.error("Login failed:", result.error);
+    } else {
+      // Redirect after successful login
+      router.push("/");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="mt-32"
+    >
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Sign In</button>
+    </form>
+  );
+}
