6c2c4e037259b681569527e6bd77997d340b9820
theabhipatel <theabhipatel.co@gmail.com>
2024-09-30 14:40:54 +0530

    feat added credential provider in auth.ts to use local email password login

--- auth.ts
+++ auth.ts
@@ -1,6 +1,27 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    GoogleProvider,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { type: "text" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        const user = { email: "abhi@gmail.com", password: "1234" };
+        if (credentials.email === user.email && credentials.password === user.password) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
 });
