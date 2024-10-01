1c42ef231987a9cc025a6331b676ac16b3df764a
theabhipatel <theabhipatel.co@gmail.com>
2024-10-01 11:26:38 +0530

    design signin page layout

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,15 +1,28 @@
 "use client";
-
 import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleCredentialSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     const result = await signIn("credentials", {
@@ -18,8 +31,10 @@ export default function SignIn() {
       redirect: false,
     });
 
+    // console.log("result ------->", result);
+
     if (result?.error) {
-      //   console.error("Login failed:", result.error);
+      // console.error("Login failed:", result.error);
     } else {
       // Redirect after successful login
       router.push("/");
@@ -27,29 +42,71 @@ export default function SignIn() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="mt-32"
-    >
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign In</button>
-    </form>
+    <div className="container mb-20 mt-40 flex justify-center">
+      <Card className="w-[400px] border-muted-foreground/50 bg-background p-5 text-foreground backdrop:blur-sm">
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl">Developers blog</CardTitle>
+          <CardDescription className="tracking-wider">Sign in to your account.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleCredentialSubmit}>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your Email"
+                  className="border-muted-foreground focus:border-muted"
+                />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter your Password"
+                  className="border-muted-foreground focus:border-muted"
+                />
+              </div>
+              <div>
+                <Button
+                  type="submit"
+                  className="w-full"
+                >
+                  Sign in
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-2">
+          <p className="text-sm text-muted-foreground">Or Signin with</p>
+          <Button
+            variant={"outline"}
+            className="w-full gap-3"
+          >
+            <FcGoogle className="text-lg" /> Google
+          </Button>
+          <Button
+            variant={"outline"}
+            className="w-full gap-3"
+          >
+            <FaGithub className="text-lg" /> Github
+          </Button>
+          <p className="text-sm text-muted-foreground">
+            Don&apos;t have an account ?{" "}
+            <Link
+              href={"/auth/signup"}
+              className="text-primary-foreground"
+            >
+              Sign up
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
