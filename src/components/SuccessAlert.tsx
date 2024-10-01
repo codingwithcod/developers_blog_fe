4a11500b73bf0ce5775f1b862517cab0ed8d9345
theabhipatel <theabhipatel.co@gmail.com>
2024-10-01 12:04:30 +0530

    update success alert and fix close button styling issue

--- src/components/SuccessAlert.tsx
+++ src/components/SuccessAlert.tsx
@@ -12,17 +12,22 @@ interface IProps {
 const SuccessAlert: FC<IProps> = ({ onClose, title, description }) => {
   return (
     <div className="relative">
-      <Alert variant={"success"}>
+      <Alert
+        variant={"success"}
+        className="relative pr-20"
+      >
         <RocketIcon className="h-5 w-5" />
         <AlertTitle>{title}</AlertTitle>
         <AlertDescription>{description}</AlertDescription>
+        <div className="absolute right-3 top-0 flex h-full items-center justify-center">
+          <button
+            onClick={onClose}
+            className="rounded-full p-2 duration-300 hover:bg-slate-400/10"
+          >
+            <IoClose className="text-xl text-green-500/90" />
+          </button>
+        </div>
       </Alert>
-      <button
-        onClick={onClose}
-        className="absolute right-4 top-3 rounded-full p-2 duration-300 hover:bg-slate-400/10"
-      >
-        <IoClose className="text-xl text-green-500/90" />
-      </button>
     </div>
   );
 };
