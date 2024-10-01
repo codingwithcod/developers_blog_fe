6095f4c2032d721b60c8fcb66ae1d5c1006b399d
theabhipatel <theabhipatel.co@gmail.com>
2024-10-01 13:03:05 +0530

    create a config file to use all environment variable from one place

---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+/** Exporting all the environment variables. */
+
+export const AUTH_SECRET = process.env.AUTH_SECRET;
+export const AUTH_GOOGLE_ID = process.env.AUTH_GOOGLE_ID;
+export const AUTH_GOOGLE_SECRET = process.env.AUTH_GOOGLE_SECRET;
