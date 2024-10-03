776751612c8422e8ccec0630186a8344c1ec858f
theabhipatel <theabhipatel.co@gmail.com>
2024-10-03 12:27:08 +0530

    feat created a middleware for authentication on application level for all routes

---
+++ src/constant/routes.ts
@@ -0,0 +1,11 @@
+export const SIGNIN = "/auth/signin";
+export const ROOT = "/";
+
+export const PUBLIC_ROUTES = [
+  "/auth/signin",
+  "/auth/signup",
+  "/blogs",
+  "/api",
+  "/api/auth/callback/google",
+  "/api/auth/callback/github",
+];
