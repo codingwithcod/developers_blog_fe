d32c5e40789554b0d297867ba80aaed629bde34b
theabhipatel <theabhipatel.co@gmail.com>
2024-10-04 13:16:47 +0530

    update navbar styling issue in small screen and remove category nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,20 +8,19 @@ const Navbar = async () => {
   return (
     <header className="fixed top-0 z-50 mx-auto w-full border-b border-gray-200/10 bg-slate-800/50 backdrop-blur-md">
       <div className="container flex h-16 w-full justify-between">
-        <div className="flex items-center text-3xl font-semibold">
+        <div className="flex items-center text-xl font-semibold md:text-3xl">
           <Link href={"/"}>Developers Blog</Link>
         </div>
 
-        <div className="flex items-center">
+        <div className="hidden items-center md:flex">
           <div className="flex h-9 w-52 items-center rounded-full border border-gray-200/20 px-5">
             <p className="select-none text-sm text-gray-300/40">Search for anything..</p>
           </div>
         </div>
 
         <div className="flex items-center gap-5">
-          <nav className="flex items-end gap-5 text-sm text-primary-foreground">
+          <nav className="hidden items-end gap-5 text-sm text-primary-foreground sm:flex">
             <Link href={"/blogs"}>Blogs</Link>
-            <Link href={"/category"}>Category</Link>
             {session?.user ? (
               <Link href={"/new-blog"}>Write</Link>
             ) : (
