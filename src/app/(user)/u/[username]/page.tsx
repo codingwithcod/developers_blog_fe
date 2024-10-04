4e10edbdaef9438c6a008929f3c76aaaac720490
theabhipatel <theabhipatel.co@gmail.com>
2024-10-04 13:11:16 +0530

    update user profile page and used session from auth instead of useSession and updated ui for small devices

--- src/app/(user)/u/[username]/page.tsx
+++ src/app/(user)/u/[username]/page.tsx
@@ -1,13 +1,13 @@
-"use client";
 import SignOutButton from "@/components/SignOutButton";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { useSession } from "next-auth/react";
 import { notFound } from "next/navigation";
 import { FC } from "react";
 import { blogs as constantBlogs } from "@/constant/blogs";
 import { IBlog } from "@/interfaces/blog";
 import BlogCard from "@/components/BlogCard";
+import { auth } from "@/auth";
+import { BsThreeDots } from "react-icons/bs";
 
 interface IProps {
   params: {
@@ -15,27 +15,57 @@ interface IProps {
   };
 }
 
-const Profile: FC<IProps> = ({ params: { username } }) => {
+const Profile: FC<IProps> = async ({ params: { username } }) => {
+  const session = await auth();
   const decodedUsername = decodeURIComponent(username);
-
-  const { data: session } = useSession();
 
   if (!decodedUsername.startsWith("@")) {
     return notFound();
   }
 
   const localBlogs = [
-    ...(JSON.parse(localStorage.getItem("blogs") ?? "[]") as IBlog[]),
+    // ...(JSON.parse(localStorage.getItem("blogs") ?? "[]") as IBlog[]),
     ...constantBlogs,
   ] as IBlog[];
 
   return (
     <div className="container flex h-full pt-16">
       {/* ---> Left side */}
-      <div className="flex h-full w-[70%] flex-col pr-20">
+      <div className="flex h-full w-full flex-col md:w-[75%] md:pr-20 lg:w-[70%]">
         {/* ---> Header */}
         <div className="border-b py-10">
-          <span className="text-4xl font-semibold tracking-wide">{session?.user?.name}</span>
+          {/* ---> For large screens */}
+          <div className="hidden items-center justify-between md:flex">
+            <span className="text-4xl font-semibold tracking-wide">{session?.user?.name}</span>
+            <BsThreeDots className="text-xl" />
+          </div>
+          {/* ---> For small screens */}
+          <div className="flex items-center justify-between md:hidden">
+            <div className="flex items-center gap-5">
+              <Avatar className="h-16 w-16">
+                <AvatarImage src={session?.user?.image ?? ""} />
+                <AvatarFallback className="bg-indigo-500 text-2xl font-bold capitalize">
+                  {session?.user?.name?.slice(0, 2)}
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <h2 className="text-xl font-semibold tracking-wider sm:text-2xl">
+                  {session?.user?.name}
+                </h2>
+                <p className="text-muted-foreground">552 Followers</p>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-3">
+              <Button
+                variant={"outline"}
+                className="rounded-full"
+              >
+                Follow
+              </Button>
+              <BsThreeDots className="text-xl" />
+            </div>
+          </div>
         </div>
         {/* ---> Body */}
 
@@ -51,10 +81,10 @@ const Profile: FC<IProps> = ({ params: { username } }) => {
         </div>
       </div>
       {/* ---> Right side */}
-      <div className="sticky top-16 flex h-[90vh] w-[30%] flex-col gap-3 border-l border-border/70 p-10">
-        <Avatar className="h-20 w-20 border">
+      <div className="sticky top-16 hidden h-[90vh] w-[25%] flex-col gap-3 border-l border-border/70 p-5 md:flex lg:w-[30%] lg:p-10">
+        <Avatar className="h-20 w-20">
           <AvatarImage src={session?.user?.image ?? ""} />
-          <AvatarFallback className="bg-background text-2xl font-bold capitalize text-orange-500">
+          <AvatarFallback className="bg-indigo-500 text-2xl font-bold capitalize">
             {session?.user?.name?.slice(0, 2)}
           </AvatarFallback>
         </Avatar>
@@ -80,13 +110,3 @@ const Profile: FC<IProps> = ({ params: { username } }) => {
 };
 
 export default Profile;
-
-/** ---> Previous code for this file for future use */
-{
-  /* <div className="flex flex-col items-center gap-4">
-<h1 className="text-2xl font-semibold tracking-widest">Welcome</h1>
-<p className="text-4xl font-bold">{session?.user?.name}</p>
-
-<SignOutButton />
-</div> */
-}
