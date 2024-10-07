711a1edd6b2fc1c1289dcebe056409946ee82ecc
theabhipatel <theabhipatel.co@gmail.com>
2024-10-07 14:48:42 +0530

    redesign view blog page and added title , user profile, user name, reads, likes and comments icons

--- src/app/(public)/blogs/[slug]/page.tsx
+++ src/app/(public)/blogs/[slug]/page.tsx
@@ -4,6 +4,12 @@ import Link from "next/link";
 import MDEditor from "@uiw/react-md-editor";
 import { blogs } from "@/constant/blogs";
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { BsThreeDots } from "react-icons/bs";
+import { DotFilledIcon } from "@radix-ui/react-icons";
+import { FaBookReader, FaCommentDots } from "react-icons/fa";
+import { AiFillLike } from "react-icons/ai";
 
 interface IProps {
   params: {
@@ -20,34 +26,76 @@ const Blog: FC<IProps> = ({ params: { slug } }) => {
   const blog = blogs.find((blog) => blog.slug === slug);
 
   return (
-    <div className="container flex min-h-[70vh] w-full flex-col px-32 pt-20 sm:px-5 md:px-10 lg:px-40">
+    <div className="container flex min-h-[70vh] w-full flex-col px-32 pt-20 sm:px-5 md:px-10 lg:px-52">
       {!blog && (
         <div className="flex h-[60vh] items-center justify-center">
-          <p className="text-muted-foreground">This Blog is not exist or unavailable</p>
+          <p className="text-muted-foreground">This Blog is not exists or unavailable</p>
         </div>
       )}
       {blog && (
         <>
+          {/* ---> Blog header  */}
           <div>
-            <p className="text-2xl font-semibold tracking-widest">
-              <Link
-                href={`/blogs`}
-                className="duration-300 hover:text-blue-400 hover:underline"
-              >
-                Blogs
-              </Link>
-              / <span className="font-bold">{blog.title}</span>
-            </p>
-            <div className="flex items-center justify-center py-5">
-              <div className="h-[20rem]">
-                <Image
-                  src={blog.thumbnail}
-                  height={400}
-                  width={400}
-                  alt={blog.title}
-                  className="h-full w-full rounded-lg object-contain"
-                />
+            <h1 className="py-5 text-4xl font-bold tracking-wider">{blog.title}</h1>
+
+            <div className="flex items-center justify-between py-5">
+              <div className="flex items-center gap-5">
+                <Link href={`/u/@theabhipatel`}>
+                  <Avatar className="h-12 w-12">
+                    <AvatarImage src={""} />
+                    <AvatarFallback className="bg-indigo-500 text-xl font-bold capitalize text-white">
+                      {blog.userName.slice(0, 1)}
+                    </AvatarFallback>
+                  </Avatar>
+                </Link>
+                <div>
+                  <p className="text-base font-semibold tracking-wider sm:text-2xl">
+                    <Link href={`/u/@theabhipatel`}>{blog.userName}</Link>
+                  </p>
+                  <div className="flex items-center gap-1">
+                    <p className="text-sm text-muted-foreground">6 min read</p>
+                    <DotFilledIcon className="text-xs text-muted-foreground" />
+                    <p className="text-sm text-muted-foreground">{blog.createdAt.toDateString()}</p>
+                  </div>
+                </div>
               </div>
+
+              <div className="flex items-center gap-3">
+                <Button
+                  variant={"outline"}
+                  className="rounded-full"
+                >
+                  Follow
+                </Button>
+                <BsThreeDots className="text-xl" />
+              </div>
+            </div>
+
+            <div className="flex h-12 items-center gap-5 border-y border-muted-foreground/30 py-2">
+              <div className="flex items-center gap-3">
+                <FaBookReader /> {blog.reads}k reads
+              </div>
+              <div className="h-full border-r border-muted-foreground/30" />
+              <div className="flex items-center gap-3">
+                <AiFillLike /> 400
+              </div>
+              <div className="h-full border-r border-muted-foreground/30" />
+              <div className="flex items-center gap-3">
+                <FaCommentDots /> 120
+              </div>
+            </div>
+          </div>
+
+          {/* ---> Blog thumbnail */}
+          <div className="flex items-center justify-center py-5">
+            <div className="h-[20rem]">
+              <Image
+                src={blog.thumbnail}
+                height={400}
+                width={400}
+                alt={blog.title}
+                className="h-full w-full rounded-lg object-contain"
+              />
             </div>
           </div>
 
