d9d091c1bbb8cd67f47353c2d247bf56e59ee8d2
theabhipatel <theabhipatel.co@gmail.com>
2024-10-07 13:11:16 +0530

    fix height issue in home page hero section after adding padding top instead of margin

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 
 const HeroSection = () => {
   return (
-    <section className="flex h-[18rem] w-full flex-col items-center gap-2 pt-52 text-center sm:h-[25rem]">
+    <section className="flex h-[32rem] w-full flex-col items-center gap-2 pt-52 text-center sm:h-[36rem]">
       <h1 className="text-4xl font-semibold sm:text-5xl md:text-7xl">Developers Blog</h1>
       <p className="text-[10px] tracking-[0.34em] sm:text-sm md:text-lg">
         For Developers by Developers
