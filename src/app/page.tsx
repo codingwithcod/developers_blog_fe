956f2e236fa40fe7f7dbb49c10e445b037673f15
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-08 20:52:00 +0530

    update background gradient on home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import MostReadBlogsSection from "@/components/home/MostReadBlogsSection";
 const Home = () => {
   return (
     <>
-      <div className="absolute top-0 -z-50 h-full w-full from-[#27235399] to-black dark:bg-gradient-to-b"></div>
-      <main className="container min-h-dvh bg-background sm:px-5 md:px-10 lg:px-20">
+      <div className="absolute top-0 -z-50 h-full w-full bg-gradient-to-b from-indigo-50 via-background to-background dark:from-[#090624]" />
+      <main className="container min-h-dvh sm:px-5 md:px-10 lg:px-20">
         <HeroSection />
         <MostReadBlogsSection />
       </main>
