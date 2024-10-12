d29bf8e2d7328282b3aa43991b4c787e25b5d9a3
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-12 13:12:03 +0530

    add api base url variable in config as well as in .env.example

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,15 @@
 /** Exporting all the environment variables. */
+// ---> Base urls
+export const APP_BASE_URL = process.env.APP_BASE_URL;
+export const API_BASE_URL = process.env.API_BASE_URL;
 
+// ---> Next auth secret
 export const AUTH_SECRET = process.env.AUTH_SECRET;
 
+// ---> Google secret
 export const AUTH_GOOGLE_CLIENT_ID = process.env.AUTH_GOOGLE_CLIENT_ID;
 export const AUTH_GOOGLE_SECRET = process.env.AUTH_GOOGLE_SECRET;
 
+// ---> Github secret
 export const AUTH_GITHUB_CLIENT_ID = process.env.AUTH_GITHUB_CLIENT_ID;
 export const AUTH_GITHUB_SECRET = process.env.AUTH_GITHUB_SECRET;
