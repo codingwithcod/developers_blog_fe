468521be0d735cb237e7518b94bfd8da351dfd50
theabhipatel <theabhipatel.co@gmail.com>
2024-10-15 19:41:28 +0530

    feat created credentials signin working with database and updated token and session

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { CredentialsSignin } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -8,6 +8,8 @@ import {
   AUTH_GOOGLE_CLIENT_ID,
   AUTH_GOOGLE_SECRET,
 } from "@/config";
+import { axiosInstance } from "./utils/axiosInstance";
+import { AxiosError } from "axios";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
@@ -39,15 +41,44 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: { type: "password" },
       },
       async authorize(credentials) {
-        const user = { email: "abhi@gmail.com", password: "1234", name: "TheabhiPatel" };
-        if (credentials.email === user.email && credentials.password === user.password) {
-          return user;
+        const { email, password } = credentials;
+        try {
+          const res = await axiosInstance.post("/auth/signin", { email, password });
+          return { ...res.data.user, accessToken: res.data.accessToken };
+        } catch (error) {
+          if (error instanceof AxiosError) {
+            throw new CredentialsSignin(error.response?.data.message);
+          }
+          throw new Error("Invalid email or password.");
         }
-
-        return null;
       },
     }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user && user.email) {
+        token._id = user._id;
+        token.email = user.email;
+        token.firstName = user.firstName;
+        token.lastName = user.lastName;
+        token.role = user.role;
+        token.profilePic = user.profilePic;
+        token.accessToken = user.accessToken;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      const { _id, firstName, lastName, email, role, profilePic, accessToken } = token;
+      session.user._id = _id;
+      session.user.firstName = firstName;
+      session.user.lastName = lastName;
+      session.user.email = email;
+      session.user.role = role;
+      session.user.profilePic = profilePic;
+      session.user.accessToken = accessToken;
+      return session;
+    },
+  },
   pages: {
     signIn: "/auth/signin",
   },
