ef7bbdc1ddf22194d261724e83ca461715eccfeb
theabhipatel <theabhipatel.co@gmail.com>
2024-10-15 19:42:55 +0530

    create auth action for credential signin with server action

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -1,5 +1,31 @@
 "use server";
-import { signOut } from "@/auth";
+import { signIn, signOut } from "@/auth";
+import { AuthError } from "next-auth";
+
+export async function handleCredentialsSignin({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  try {
+    await signIn("credentials", { email, password, redirectTo: "/" });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      if (error.type === "CredentialsSignin") {
+        return {
+          message: "Invalid credentials",
+        };
+      } else {
+        return {
+          message: "Something went wrong.",
+        };
+      }
+    }
+    throw error;
+  }
+}
 
 export async function handleSignOut() {
   await signOut({ redirectTo: "/auth/signin" });
