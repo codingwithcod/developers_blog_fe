3029372974fdb84db1a62cd931e54b02e3d90081
theabhipatel <theabhipatel.co@gmail.com>
2024-10-17 17:32:33 +0530

    update user profile page and used firstName, and lastName instead of name and also profilePic from database

--- src/app/(user)/u/[username]/page.tsx
+++ src/app/(user)/u/[username]/page.tsx
@@ -84,13 +84,15 @@ const Profile: FC<IProps> = async ({ params: { username } }) => {
       {/* ---> Right side */}
       <div className="sticky top-16 hidden h-[90vh] w-[25%] flex-col gap-3 border-l border-muted-foreground/20 p-5 md:flex lg:p-10">
         <Avatar className="h-20 w-20">
-          <AvatarImage src={session?.user?.image ?? ""} />
+          <AvatarImage src={session?.user?.profilePic ?? ""} />
           <AvatarFallback className="bg-indigo-500 text-2xl font-bold capitalize text-white">
-            {session?.user?.name?.slice(0, 2)}
+            {session?.user?.firstName?.slice(0, 1)}
           </AvatarFallback>
         </Avatar>
         <div className="flex flex-col gap-2">
-          <h2 className="font-medium tracking-wider text-foreground">{session?.user?.name}</h2>
+          <h2 className="font-medium tracking-wider text-foreground">
+            {session?.user?.firstName} {session?.user?.lastName}
+          </h2>
           <p className="text-muted-foreground">552 Followers</p>
           <p className="text-muted-foreground">Creator, Software Engineer, Traveller.</p>
           <div>
