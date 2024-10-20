aadec4b7dc505e3e0b7d9888d174eeb2219b84f7
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-20 21:19:41 +0530

    feat created an axios instanced axiosClient and created a inteceptor to dynamically pass the accessToken from the session

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,6 +1,26 @@
-import { API_BASE_URL } from "@/config";
+import { NEXT_PUBLIC_API_BASE_URL } from "@/config";
 import axios from "axios";
+import { getToken } from "next-auth/jwt";
+import { getSession } from "next-auth/react";
 
-export const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+export const axiosClient = axios.create({
+  baseURL: NEXT_PUBLIC_API_BASE_URL,
+});
+
+async function getAuthToken(req?: Request | { headers: Headers | Record<string, string> }) {
+  if (typeof window === "undefined" && req) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    return token?.accessToken;
+  } else {
+    const session = await getSession();
+    return session?.user?.accessToken;
+  }
+}
+
+axiosClient.interceptors.request.use(async (req) => {
+  const token = await getAuthToken();
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
 });
