29a5d4024ef2a575965d4e0833518d7158677481
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-20 21:08:35 +0530

    update api base url variable name and prefix next_public to expose env varialbe to public client side apis

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 /** Exporting all the environment variables. */
 // ---> Base urls
 export const APP_BASE_URL = process.env.APP_BASE_URL;
-export const API_BASE_URL = process.env.API_BASE_URL;
+export const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // ---> Next auth secret
 export const AUTH_SECRET = process.env.AUTH_SECRET;
