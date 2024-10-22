7bfe5ee7c969d07935bff7becf2673d3eb3ea38e
theabhipatel <theabhipatel.co@gmail.com>
2024-10-22 19:47:20 +0530

    feat integrated api for fetch all blogs and create page dynamic

--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -1,46 +1,58 @@
-"use client";
 import React from "react";
 import { IBlog } from "@/interfaces/blog";
 import BlogCard from "@/components/BlogCard";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { HiPencilSquare } from "react-icons/hi2";
-import { blogs as constantBlogs } from "@/constant/blogs";
+import { axiosClient } from "@/utils/axiosClient";
+import apiEndpoints from "@/api/apiEndpoints";
+import { errorLog } from "@/utils/errorLog";
 
-const Blogs = () => {
-  // ...(JSON.parse(localStorage.getItem("blogs") ?? "[]") as IBlog[]),
-  const localBlogs = [...constantBlogs] as IBlog[];
-
-  return (
-    <div className="container flex min-h-[90vh] flex-col items-center pb-20 pt-24 sm:px-5 md:px-10 lg:px-20">
-      {localBlogs.length === 0 && (
-        <div className="flex h-full w-full flex-col items-center justify-end">
-          <p className="text-2xl">There is no blogs available</p>
-          <p className="text-muted-foreground">You can also Write your own blogs</p>
-          <Link href={`/new-blog`}>
-            <Button
-              variant={"outline"}
-              size={"sm"}
-              className="mt-4 w-20 gap-1 rounded-full"
-            >
-              <HiPencilSquare className="text-sm" />
-              Write
-            </Button>
-          </Link>
+const Blogs = async () => {
+  try {
+    const res = await axiosClient.get(apiEndpoints.blogs.getAllBlogs);
+    const blogs = res.data.blogs as IBlog[];
+    return (
+      <div className="container flex min-h-[90vh] flex-col items-center pb-20 pt-24 sm:px-5 md:px-10 lg:px-20">
+        {blogs.length === 0 && (
+          <div className="flex h-full w-full flex-col items-center justify-end">
+            <p className="text-2xl">There is no blogs available</p>
+            <p className="text-muted-foreground">You can also Write your own blogs</p>
+            <Link href={`/new-blog`}>
+              <Button
+                variant={"outline"}
+                size={"sm"}
+                className="mt-4 w-20 gap-1 rounded-full"
+              >
+                <HiPencilSquare className="text-sm" />
+                Write
+              </Button>
+            </Link>
+          </div>
+        )}
+        <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+          {blogs.map((blog) => {
+            return (
+              <BlogCard
+                blog={blog}
+                key={blog.slug}
+              />
+            );
+          })}
         </div>
-      )}
-      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-        {localBlogs.map((blog) => {
-          return (
-            <BlogCard
-              blog={blog}
-              key={blog.id}
-            />
-          );
-        })}
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    errorLog(error);
+    return (
+      <div className="container flex min-h-[90vh] flex-col items-center justify-center pb-20 pt-24 text-center sm:px-5 md:px-10 lg:px-20">
+        <h1 className="text-lg">Failed to fetch blogs</h1>
+        <p className="text-sm text-muted-foreground">
+          There was an issue fetching the blogs. Please try again later.
+        </p>
+      </div>
+    );
+  }
 };
 
 export default Blogs;
