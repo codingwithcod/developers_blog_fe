3378150b0cb04eba4494c14555ef22a8561fdcc6
theabhipatel <theabhipatel.co@gmail.com>
2024-10-25 18:20:41 +0530

    fix access token not getting on server side axios api call

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,6 +1,6 @@
+import { auth } from "@/auth";
 import { NEXT_PUBLIC_API_BASE_URL } from "@/config";
 import axios from "axios";
-import { getToken } from "next-auth/jwt";
 
 export const axiosClient = axios.create({
   baseURL: NEXT_PUBLIC_API_BASE_URL,
@@ -8,8 +8,8 @@ export const axiosClient = axios.create({
 
 async function getAuthToken(req?: Request | { headers: Headers | Record<string, string> }) {
   if (typeof window === "undefined" && req) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    return token?.accessToken;
+    const session = await auth();
+    return session?.user.accessToken;
   } else if (typeof window !== "undefined" && !req) {
     const { getSession } = await import("next-auth/react");
     const session = await getSession();
@@ -18,7 +18,7 @@ async function getAuthToken(req?: Request | { headers: Headers | Record<string, 
 }
 
 axiosClient.interceptors.request.use(async (req) => {
-  const token = await getAuthToken();
+  const token = typeof window === "undefined" ? await getAuthToken(req) : await getAuthToken();
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
