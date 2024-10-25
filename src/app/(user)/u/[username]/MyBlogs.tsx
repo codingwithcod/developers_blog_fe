5ea08f66146acdaea0c15b2b0d036144d1f23555
theabhipatel <theabhipatel.co@gmail.com>
2024-10-25 18:37:34 +0530

    refactor created a separate component for myblogs on user profile page

---
+++ src/app/(user)/u/[username]/MyBlogs.tsx
@@ -0,0 +1,62 @@
+import apiEndpoints from "@/api/apiEndpoints";
+import BlogCard from "@/components/BlogCard";
+import { Button } from "@/components/ui/button";
+import { IBlog } from "@/interfaces/IBlog";
+import { axiosClient } from "@/utils/axiosClient";
+import { errorLog } from "@/utils/errorLog";
+import Link from "next/link";
+import React from "react";
+import { HiPencilSquare } from "react-icons/hi2";
+
+const MyBlogs = async () => {
+  try {
+    const res = await axiosClient.get(apiEndpoints.blogs.getAllMyBlogs);
+    const blogs = res.data.blogs as IBlog[];
+
+    return (
+      <>
+        {blogs.length === 0 && (
+          <div className="flex h-[20rem] w-full flex-col items-center justify-center">
+            <p className="text-2xl text-muted-foreground">There is no blogs available</p>
+            <p className="text-sm text-muted-foreground/70">You can also Write your own blogs</p>
+            <Link href={`/new-blog`}>
+              <Button
+                variant={"outline"}
+                size={"sm"}
+                className="mt-4 w-20 gap-1 rounded-full"
+              >
+                <HiPencilSquare className="text-sm" />
+                Write
+              </Button>
+            </Link>
+          </div>
+        )}
+
+        {blogs.length > 0 && (
+          <div className="grid grid-cols-1 gap-10 py-16 sm:grid-cols-2 xl:grid-cols-3">
+            {blogs.map((blog) => {
+              return (
+                <BlogCard
+                  blog={blog}
+                  key={blog._id}
+                />
+              );
+            })}
+          </div>
+        )}
+      </>
+    );
+  } catch (error) {
+    errorLog(error);
+    return (
+      <div className="container flex min-h-[90vh] flex-col items-center justify-center pb-20 pt-24 text-center sm:px-5 md:px-10 lg:px-20">
+        <h1 className="text-lg">Failed to fetch blogs</h1>
+        <p className="text-sm text-muted-foreground">
+          There was an issue fetching the blogs. Please try again later.
+        </p>
+      </div>
+    );
+  }
+};
+
+export default MyBlogs;
--- src/app/(user)/u/[username]/page.tsx
+++ src/app/(user)/u/[username]/page.tsx
@@ -3,14 +3,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { notFound } from "next/navigation";
 import { FC } from "react";
-import { IBlog } from "@/interfaces/IBlog";
-import BlogCard from "@/components/BlogCard";
 import { auth } from "@/auth";
 import { BsThreeDots } from "react-icons/bs";
 import ThemeToggleButton from "@/components/ThemeToggleButton";
-import { axiosClient } from "@/utils/axiosClient";
-import apiEndpoints from "@/api/apiEndpoints";
-import { errorLog } from "@/utils/errorLog";
+import MyBlogs from "./MyBlogs";
 
 interface IProps {
   params: {
@@ -26,103 +22,79 @@ const Profile: FC<IProps> = async ({ params: { username } }) => {
     return notFound();
   }
 
-  try {
-    const res = await axiosClient.get(apiEndpoints.blogs.getAllMyBlogs);
-    const blogs = res.data.blogs as IBlog[];
-
-    return (
-      <div className="container flex h-full pt-16 sm:px-5 md:px-10 lg:px-20">
-        {/* ---> Left side */}
-        <div className="flex h-full w-full flex-col md:w-[75%] md:pr-20">
-          {/* ---> Header */}
-          <div className="border-b border-muted-foreground/20 py-10">
-            {/* ---> For large screens header */}
-            <div className="hidden items-center justify-between md:flex">
-              <span className="text-4xl font-semibold tracking-wide">{session?.user?.name}</span>
-              <BsThreeDots className="text-xl" />
-            </div>
-            {/* ---> For small screens header */}
-            <div className="flex items-center justify-between md:hidden">
-              <div className="flex items-center gap-5">
-                <Avatar className="h-16 w-16">
-                  <AvatarImage src={session?.user?.image ?? ""} />
-                  <AvatarFallback className="bg-indigo-500 text-2xl font-bold capitalize text-white">
-                    {session?.user?.name?.slice(0, 2)}
-                  </AvatarFallback>
-                </Avatar>
-                <div>
-                  <h2 className="text-xl font-semibold tracking-wider sm:text-2xl">
-                    {session?.user?.name}
-                  </h2>
-                  <p className="text-muted-foreground">552 Followers</p>
-                </div>
-              </div>
-
-              <div className="flex items-center gap-3">
-                <Button
-                  variant={"outline"}
-                  className="rounded-full"
-                >
-                  Follow
-                </Button>
-                <BsThreeDots className="text-xl" />
+  return (
+    <div className="container flex h-full pt-16 sm:px-5 md:px-10 lg:px-20">
+      {/* ---> Left side */}
+      <div className="flex h-full w-full flex-col md:w-[75%] md:pr-20">
+        {/* ---> Header */}
+        <div className="border-b border-muted-foreground/20 py-10">
+          {/* ---> For large screens header */}
+          <div className="hidden items-center justify-between md:flex">
+            <span className="text-4xl font-semibold tracking-wide">{session?.user?.name}</span>
+            <BsThreeDots className="text-xl" />
+          </div>
+          {/* ---> For small screens header */}
+          <div className="flex items-center justify-between md:hidden">
+            <div className="flex items-center gap-5">
+              <Avatar className="h-16 w-16">
+                <AvatarImage src={session?.user?.image ?? ""} />
+                <AvatarFallback className="bg-indigo-500 text-2xl font-bold capitalize text-white">
+                  {session?.user?.name?.slice(0, 2)}
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <h2 className="text-xl font-semibold tracking-wider sm:text-2xl">
+                  {session?.user?.name}
+                </h2>
+                <p className="text-muted-foreground">552 Followers</p>
               </div>
             </div>
-          </div>
-          {/* ---> Body */}
-          <div className="grid grid-cols-1 gap-10 py-16 sm:grid-cols-2 xl:grid-cols-3">
-            {blogs.map((blog) => {
-              return (
-                <BlogCard
-                  blog={blog}
-                  key={blog._id}
-                />
-              );
-            })}
-          </div>
-        </div>
 
-        {/* ---> Right side */}
-        <div className="sticky top-16 hidden h-[90vh] w-[25%] flex-col gap-3 border-l border-muted-foreground/20 p-5 md:flex lg:p-10">
-          <Avatar className="h-20 w-20">
-            <AvatarImage src={session?.user?.profilePic ?? ""} />
-            <AvatarFallback className="bg-indigo-500 text-2xl font-bold capitalize text-white">
-              {session?.user?.firstName?.slice(0, 1)}
-            </AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col gap-2">
-            <h2 className="font-medium tracking-wider text-foreground">
-              {session?.user?.firstName} {session?.user?.lastName}
-            </h2>
-            <p className="text-muted-foreground">552 Followers</p>
-            <p className="text-muted-foreground">Creator, Software Engineer, Traveller.</p>
-            <div>
+            <div className="flex items-center gap-3">
               <Button
                 variant={"outline"}
                 className="rounded-full"
               >
                 Follow
               </Button>
+              <BsThreeDots className="text-xl" />
             </div>
           </div>
-          <div className="flex h-full items-end justify-between">
-            <SignOutButton />
-            <ThemeToggleButton />
+        </div>
+        {/* ---> Body */}
+        <MyBlogs />
+      </div>
+
+      {/* ---> Right side */}
+      <div className="sticky top-16 hidden h-[90vh] w-[25%] flex-col gap-3 border-l border-muted-foreground/20 p-5 md:flex lg:p-10">
+        <Avatar className="h-20 w-20">
+          <AvatarImage src={session?.user?.profilePic ?? ""} />
+          <AvatarFallback className="bg-indigo-500 text-2xl font-bold capitalize text-white">
+            {session?.user?.firstName?.slice(0, 1)}
+          </AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col gap-2">
+          <h2 className="font-medium tracking-wider text-foreground">
+            {session?.user?.firstName} {session?.user?.lastName}
+          </h2>
+          <p className="text-muted-foreground">552 Followers</p>
+          <p className="text-muted-foreground">Creator, Software Engineer, Traveller.</p>
+          <div>
+            <Button
+              variant={"outline"}
+              className="rounded-full"
+            >
+              Follow
+            </Button>
           </div>
         </div>
+        <div className="flex h-full items-end justify-between">
+          <SignOutButton />
+          <ThemeToggleButton />
+        </div>
       </div>
-    );
-  } catch (error) {
-    errorLog(error);
-    return (
-      <div className="container flex min-h-[90vh] flex-col items-center justify-center pb-20 pt-24 text-center sm:px-5 md:px-10 lg:px-20">
-        <h1 className="text-lg">Failed to fetch blogs</h1>
-        <p className="text-sm text-muted-foreground">
-          There was an issue fetching the blogs. Please try again later.
-        </p>
-      </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Profile;
