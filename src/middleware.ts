59dc89d34c38e7131fdbb4be7c6549f7801250b1
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-10-27 13:29:27 +0530

    feat if username doesn't starts with @ then redirecting to 404 not found page

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,9 +11,18 @@ const middleware = async (request: NextRequest) => {
     PUBLIC_ROUTES.find((route) => nextUrl.pathname.startsWith(route)) || nextUrl.pathname === ROOT;
 
   const isAuthRoute = nextUrl.pathname.startsWith("/auth");
+  const isUserProfileRoute = nextUrl.pathname.startsWith("/u");
 
+  /** ---> If user not logged in and try to access private pages. */
   if (!isAuthenticated && !isPublicRoute) return Response.redirect(new URL(SIGNIN, nextUrl));
+
+  /** ---> If user logged in and try to access auth pages. */
   if (isAuthenticated && isAuthRoute) return Response.redirect(new URL(ROOT, nextUrl));
+
+  /** ---> If username not starts with @ then redirecting to 404 not found page. */
+  if (isUserProfileRoute && !nextUrl.pathname.split("/u/")[1].startsWith("@")) {
+    return NextResponse.rewrite(new URL("/404", nextUrl));
+  }
 
   NextResponse.next();
 };
