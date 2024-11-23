c49330befba2e861790d0b2732c57a7f633070f3
theabhipatel <theabhipatel.co@gmail.com>
2024-11-23 17:57:09 +0530

    add metadata in new blog and update blog page

--- src/app/(user)/blog/new/page.tsx
+++ src/app/(user)/blog/new/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import AddUpdateBlog from "../AddUpdateBlog";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Write new blog | Developers Blog",
+  description: "Write your blog with markdown and publish them to public.",
+};
 
 const NewBlog = () => {
   return <AddUpdateBlog />;
--- src/app/(user)/blog/update/page.tsx
+++ src/app/(user)/blog/update/page.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import AddUpdateBlog from "../AddUpdateBlog";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update blog | Developers Blog",
+  description: "Update your blog and publish them to direct public or save as draft.",
+};
 
 const UpdateBlog = () => {
   return <AddUpdateBlog />;
