e8378e781b6e7c485f478b5841bf1e84730d1b03
theabhipatel <theabhipatel.co@gmail.com>
2024-11-23 18:18:04 +0530

    chore created update blog page to acceps params

--- src/app/(user)/blog/update/[blogId]/page.tsx
+++ src/app/(user)/blog/update/[blogId]/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import AddUpdateBlog from "../AddUpdateBlog";
+import AddUpdateBlog from "../../AddUpdateBlog";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
