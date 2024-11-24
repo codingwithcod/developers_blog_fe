7f260ae029f1245f0a99dcbc8fa93e0bf8c877ff
TheAbhiPatel <theabhipatel.co@gmail.com>
2024-11-24 21:30:10 +0530

    udpate dashboar component and added blog, published, and draft blog count for future reference

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -17,12 +17,21 @@ const Dashboard = () => {
       </header>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="flex aspect-video flex-col items-center justify-center rounded-xl bg-muted/50">
+            <span className="text-5xl font-semibold">50</span>
+            <p className="text-xl">Blogs</p>
+          </div>
+          <div className="flex aspect-video flex-col items-center justify-center rounded-xl bg-muted/50">
+            <span className="text-5xl font-semibold">42</span>
+            <p className="text-xl text-green-500">Published</p>
+          </div>
+          <div className="flex aspect-video flex-col items-center justify-center rounded-xl bg-muted/50">
+            <span className="text-5xl font-semibold">18</span>
+            <p className="text-xl text-yellow-500">Draft</p>
+          </div>
         </div>
         <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        <div className="h-[10rem] rounded-xl bg-red-500/20" />
+        <div className="h-[10rem] rounded-xl bg-red-500/10" />
         <div className="h-[10rem] rounded-xl bg-muted/50" />
         <div className="h-[10rem] rounded-xl bg-muted/50" />
         <div className="h-[10rem] rounded-xl bg-muted/50" />
